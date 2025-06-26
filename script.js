0d1f0f10e9eea8ba42b32eb57e2fdc2ff50bfa07
Manas <151569630+Manas-Kalamkar@users.noreply.github.com>
2025-06-26 22:39:55 +0530

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const cityInput = document.getElementById('city');
 const apiKey = 'a0824c7a15850d3021f120f4201ffac6'; 
 
 //getting the data for main index page using cityInput and local storage
-if(window.location.pathname.includes('index.html') || window.location.pathname === ('/') || window.location.pathname.includes('project_4_weather')){
+if(window.location.pathname.includes('index.html') || window.location.pathname === ('/') )){
 
     if(cityInput){
 cityInput.addEventListener('keypress', async (e) => {  
